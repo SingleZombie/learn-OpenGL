d9a735b81a58a2903fe346229f79ec0c51aba0c6
SingleZombie <singlezombie@163.com>
2019-05-27 11:01:39 +0800

    finish lesson6, clear up the file

---
+++ Nehe/lesson6/MyVertex.h
@@ -0,0 +1,37 @@
+#ifndef MY_VERTEX_H
+#define MY_VERTEX_H
+
+#include <cassert>
+#include <vector>
+
+namespace MyVertex
+{
+	using UINT = unsigned int;
+
+	const UINT POSITION_INDEX		= 0;
+	const UINT COLOR_INDEX			= 1;
+	const UINT TEX_COORD2_INDEX		= 2;
+	const UINT TOTAL_INDEX			= 3;
+
+	const UINT POSITION_BIT			= 1 << POSITION_INDEX;
+	const UINT COLOR_BIT			= 1 << COLOR_INDEX;
+	const UINT TEX_COORD2_BIT		= 1 << TEX_COORD2_INDEX;
+
+	class VertexProperty
+	{
+	public:
+		VertexProperty(float* value, const UINT* indice, UINT len, UINT step = 3);
+		const float& operator[](UINT index) const;
+		float& operator[](UINT index);
+		UINT size() const;
+	private:
+		float* _ptr;
+		std::vector<UINT> _indice;
+	};
+
+	using PropertyArray = std::vector<VertexProperty>;
+
+	void drawVertex(const PropertyArray& properties, UINT properBits, UINT startPos = 0, int count = -1);
+};
+
+#endif
---
+++ Nehe/lesson6/main.cpp
@@ -0,0 +1,518 @@
+#include <Windows.h>
+#include <gl\glew.h>
+#include <gl\glut.h>
+#include <iostream>
+#include "MyVertex.h"
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+HGLRC hRC = NULL;
+HDC hDC = NULL;
+HWND hwnd = NULL;
+HINSTANCE hInstance;
+
+bool keys[256];
+bool active = true; // ×îÐ¡»¯£¿
+bool fullscreen = false;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+UINT loadTexture(const char* path);
+
+void resizeGLScene(int width, int height)
+{
+	if (height == 0)
+	{
+		height = 1;
+	}
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(
+		45.0f,
+		float(width) / height,
+		0.1f,
+		100.0f
+	);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+float angle1, angle2;
+
+float vertexArr[] = 
+{
+	0.0f, 1.0f, 0.0f,		// top
+	-1.0f, -1.0f, 1.0f,		// left-down
+	-1.0f, -1.0f, -1.0f,	// left-up
+	1.0f, -1.0f, 1.0f,		// right-down
+	1.0f, -1.0f, -1.0f		// right-up
+};
+UINT vertexIndice[] = 
+{
+	0, 1, 3,
+	0, 3, 4,
+	0, 4, 2,
+	0, 2, 1
+};
+
+float vertexColor[] = 
+{
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f
+};
+
+UINT pyramidColorIndice[]
+{
+	0, 1, 2,
+	0, 2, 1,
+	0, 1, 2,
+	0, 2, 1,
+};
+
+MyVertex::PropertyArray pyramid;
+
+float cubePos[] =
+{
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f,  1.0f,
+};
+float texturePos[] = 
+{
+	0.0f, 0.0f,
+	1.0f, 0.0f,
+	1.0f, 1.0f,
+	0.0f, 1.0f
+};
+
+// clockwise
+UINT cubePosIndice[] = 
+{
+	0, 1, 2, 3,
+	4, 5, 6, 7,
+	1, 0, 7, 6,
+	3, 2, 5, 4,
+	0, 3, 4, 7,
+	2, 1, 6, 5
+};
+UINT cubeColorIndice[] =
+{
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	1, 1, 1, 1,
+	1, 1, 1, 1,
+	2, 2, 2, 2,
+	2, 2, 2, 2
+};
+UINT cubeTexCoord[] = 
+{
+	0, 1, 2, 3,
+	0, 1, 2, 3,
+	0, 1, 2, 3,
+	0, 1, 2, 3,
+	0, 1, 2, 3,
+	0, 1, 2, 3
+};
+
+
+float xrot, yrot, zrot;
+UINT containerTexture;
+
+MyVertex::PropertyArray cube;
+
+bool initGL()
+{
+	containerTexture = loadTexture("container2.png");
+	if (!containerTexture)
+	{
+		return false;
+	}
+
+	glEnable(GL_TEXTURE_2D);
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	pyramid.push_back(MyVertex::VertexProperty(vertexArr, vertexIndice, 12));
+	pyramid.push_back(MyVertex::VertexProperty(vertexColor, pyramidColorIndice, 12));
+	cube.push_back(MyVertex::VertexProperty(cubePos, cubePosIndice, 24));
+	cube.push_back(MyVertex::VertexProperty(vertexColor, cubeColorIndice, 24));
+	cube.push_back(MyVertex::VertexProperty(texturePos, cubeTexCoord, 24, 2));
+
+	return true;
+}
+
+bool drawGLScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -5.0f);
+	glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+	glRotatef(zrot, 0.0f, 0.0f, 1.0f);
+	glBindTexture(GL_TEXTURE_2D, containerTexture);
+	glBegin(GL_QUADS);
+	MyVertex::drawVertex(cube, MyVertex::POSITION_BIT | MyVertex::TEX_COORD2_BIT);
+	glEnd();
+
+	xrot += 0.03f;
+	yrot += 0.02f;
+	zrot += 0.04f;
+
+	return true;
+}
+
+void killGLWindow()
+{
+	if (fullscreen)
+	{
+		ChangeDisplaySettings(NULL, 0);
+		ShowCursor(true);
+	}
+
+	if (hRC)
+	{
+		if (!wglMakeCurrent(NULL, NULL))
+		{
+			MessageBox(NULL, "release dc & rc fail", "close error", MB_OK | MB_ICONINFORMATION);
+		}
+
+		if (!wglDeleteContext(hRC))
+		{
+			MessageBox(NULL, "release rc fail", "close error", MB_OK | MB_ICONINFORMATION);
+		}
+		hRC = NULL;
+	}
+
+	if (hDC && !ReleaseDC(hwnd, hDC))
+	{
+		MessageBox(NULL, "release dc fail", "close error", MB_OK | MB_ICONINFORMATION);
+		hDC = NULL;
+	}
+
+	if (hwnd && !DestroyWindow(hwnd))
+	{
+		MessageBox(NULL, "release window fail", "close error", MB_OK | MB_ICONINFORMATION);
+		hwnd = NULL;
+	}
+
+	if (!UnregisterClass("OpenG", hInstance))
+	{
+		MessageBox(NULL, "release window class fail", "close error", MB_OK | MB_ICONINFORMATION);
+		hInstance = NULL;
+	}
+}
+
+bool createGLWindow(const char* title, int width, int height, int bits, bool fullscreenFlag)
+{
+	GLuint pixelFormat;
+	WNDCLASS wc;
+	DWORD dwExStyle;
+	DWORD dwStyle;
+
+	RECT windowRect;
+	windowRect.left = 0;
+	windowRect.right = width;
+	windowRect.top = 0;
+	windowRect.bottom = height;
+
+	fullscreen = fullscreenFlag;
+
+	hInstance = GetModuleHandle(NULL);
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = "OpenG";
+
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(NULL, "register window fail", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	if (fullscreen)
+	{
+		DEVMODE dmScreenSettings;
+		memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
+		dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+		dmScreenSettings.dmPelsWidth = width;
+		dmScreenSettings.dmPelsHeight = height;
+		dmScreenSettings.dmBitsPerPel = bits;
+		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+		{
+			if (MessageBox(NULL, "fullscreen fail, use window mode?", "Nehe G", MB_YESNO | MB_ICONEXCLAMATION) == IDYES)
+			{
+				fullscreen = false;
+			}
+			else
+			{
+				MessageBox(NULL, "program will close", "error", MB_OK | MB_ICONSTOP);
+				return false;
+			}
+		}
+	}
+
+	if (fullscreen)
+	{
+		dwExStyle = WS_EX_APPWINDOW;
+		dwStyle = WS_POPUP;
+		ShowCursor(false);
+	}
+	else
+	{
+		dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+		dwStyle = WS_OVERLAPPEDWINDOW;
+	}
+
+	AdjustWindowRectEx(&windowRect, dwStyle, false, dwExStyle);
+
+	if (!(hwnd = CreateWindowEx(
+		dwExStyle,
+		"OpenG",
+		title,
+		WS_CLIPSIBLINGS | WS_CLIPCHILDREN | dwStyle,
+		0, 0,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL)))
+	{
+		killGLWindow();
+		MessageBox(NULL, "can't create window", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	static PIXELFORMATDESCRIPTOR pfd = 
+	{
+		sizeof(PIXELFORMATDESCRIPTOR),
+		1,
+		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER | PFD_TYPE_RGBA,
+		bits,
+		0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		0,
+		0, 0, 0, 0,
+		16,
+		0, 
+		0,
+		PFD_MAIN_PLANE,
+		0, 
+		0, 0, 0
+	};
+
+	if (!(hDC = GetDC(hwnd)))
+	{
+		killGLWindow();
+		MessageBox(NULL, "can't create pixel format", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	if (!(pixelFormat = ChoosePixelFormat(hDC, &pfd)))
+	{
+		killGLWindow();
+		MessageBox(NULL, "can't set pixel format", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	if (!(SetPixelFormat(hDC, pixelFormat, &pfd)))
+	{
+		killGLWindow();
+		MessageBox(NULL, "can't set pixel format2", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	if (!(hRC = wglCreateContext(hDC)))
+	{
+		killGLWindow();
+		MessageBox(NULL, "can't create openGL rc", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	if (!wglMakeCurrent(hDC, hRC))
+	{
+		killGLWindow();
+		MessageBox(NULL, "can't activate current openGL rc", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	ShowWindow(hwnd, SW_SHOW);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+	resizeGLScene(width, height);
+
+	if (!initGL())
+	{
+		killGLWindow();
+		MessageBox(NULL, "init fail", "error", MB_OK | MB_ICONINFORMATION);
+		return false;
+	}
+
+	return true;
+}
+
+LRESULT CALLBACK WndProc(
+	HWND hwnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam
+)
+{
+	switch (uMsg)
+	{
+	case WM_ACTIVATE:
+		if (!HIWORD(wParam))
+		{
+			active = true;
+		}
+		else
+		{
+			active = false;
+		}
+		return 0;
+		break;
+	case WM_SYSCOMMAND:
+		switch (wParam)
+		{
+		case SC_SCREENSAVE:
+		case SC_MONITORPOWER:
+			return 0;
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		return 0;
+		break;
+	case WM_KEYDOWN:
+		keys[wParam] = true;
+		return 0;
+		break;
+	case WM_KEYUP:
+		keys[wParam] = false;
+		return 0;
+		break;
+	case WM_SIZE:
+		resizeGLScene(LOWORD(lParam), HIWORD(lParam));
+		return 0;
+		break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	freopen("test.out", "w", stdout);
+
+	MSG msg;
+	bool done = false;
+	/*
+		setFullScreen
+	*/
+
+	if (!createGLWindow("Hello World", 800, 600, 16, fullscreen))
+	{
+		return 0;
+	}
+
+	while (!done)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				done = true;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (active)
+			{
+				if (keys[VK_ESCAPE])
+				{
+					done = true;
+				}
+				else
+				{
+					drawGLScene();
+					SwapBuffers(hDC);
+				}
+			}
+		}
+	}
+
+	killGLWindow();
+	return msg.wParam;
+}
+
+UINT loadTexture(const char* path)
+{
+	UINT textureID = 0;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		//glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
