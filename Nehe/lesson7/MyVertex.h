c2c0057ea21be6cb0a71848b85be2bc9779de806
SingleZombie <singlezombie@163.com>
2019-06-03 14:01:51 +0800

    finish lesson7

---
+++ Nehe/lesson7/MyVertex.h
@@ -0,0 +1,40 @@
+#ifndef MY_VERTEX_H
+#define MY_VERTEX_H
+
+#include <cassert>
+#include <vector>
+
+namespace MyVertex
+{
+	using UINT = unsigned int;
+
+	const UINT POSITION_INDEX		= 0;
+	const UINT COLOR_INDEX			= 1;
+	const UINT TEX_COORD2_INDEX		= 2;
+	const UINT NORMAL_INDEX			= 3;
+	const UINT TOTAL_INDEX			= 4;
+
+	const UINT POSITION_BIT			= 1 << POSITION_INDEX;
+	const UINT COLOR_BIT			= 1 << COLOR_INDEX;
+	const UINT TEX_COORD2_BIT		= 1 << TEX_COORD2_INDEX;
+	const UINT NORMAL_BIT			= 1 << NORMAL_INDEX;
+
+	class VertexProperty
+	{
+	public:
+		VertexProperty();
+		VertexProperty(float* value, const UINT* indice, UINT len, UINT step = 3);
+		const float& operator[](UINT index) const;
+		float& operator[](UINT index);
+		UINT size() const;
+	private:
+		float* _ptr;
+		std::vector<UINT> _indice;
+	};
+
+	using PropertyArray = std::vector<VertexProperty>;
+
+	void drawVertex(const PropertyArray& properties, UINT properBits, UINT startPos = 0, int count = -1);
+};
+
+#endif
